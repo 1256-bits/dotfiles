aaf69b325eeeb6883a8c2e3c8f70f3cb66876409
128bit <128bit@eclipso.eu>
2023-05-09 19:16:47 +0300

    add scss and sxhkdrc parsers

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-	ensure_installed = { "javascript", "typescript", "bash", "c", "lua", "vim", "vimdoc", "query", "html", },
+	ensure_installed = { "javascript", "typescript", "bash", "c", "lua", "vim", "vimdoc", "query", "html", "sxhkdrc", "scss",},
 	sync_install = false,
 	auto_install = true,
 	highlight = {
