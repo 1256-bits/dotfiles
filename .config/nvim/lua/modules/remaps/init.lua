6e09ffd8e8cd0474862d7afcde75aef59c39fb09
128bit <128bit@eclipso.eu>
2023-05-09 19:31:04 +0300

    Add the 'clear search' keybind

--- .config/nvim/lua/modules/remaps/init.lua
+++ .config/nvim/lua/modules/remaps/init.lua
@@ -1,4 +1,7 @@
 vim.g.mapleader = " "
-vim.keymap.set("n","<C-d>","<C-d>zz")
-vim.keymap.set("n","<C-u>","<C-u>zz")
-vim.keymap.set("n","<Leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<Leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<Leader>/", function()
+	vim.cmd.let("@/=''")
+end)
