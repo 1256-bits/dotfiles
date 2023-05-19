26ede3b4ea89b633096430d26af8468ef8f51cd7
128bit <128bit@eclipso.eu>
2023-05-19 14:11:03 +0300

    Change execute to vim.cmd

--- .config/nvim/lua/modules/utils/init.lua
+++ .config/nvim/lua/modules/utils/init.lua
@@ -1,9 +1,9 @@
 vim.api.nvim_create_user_command('HelpAutosplit',
     function(opts)
         if vim.fn.winwidth('%') > vim.fn.winheight('%') then
-            vim.fn.execute([[vertical help ]] .. opts.fargs[1])
+            vim.cmd([[vertical help ]] .. opts.fargs[1])
         else
-            vim.fn.execute([[help ]] .. opts.fargs[1])
+            vim.cmd([[help ]] .. opts.fargs[1])
         end
     end, { nargs = 1, complete = "help" })
 vim.cmd'cnoreabbrev h HelpAutosplit'
