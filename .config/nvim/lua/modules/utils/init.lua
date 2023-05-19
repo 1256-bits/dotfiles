26afdba5674e1dee0898220970bca0dfb9257dbe
128bit <128bit@eclipso.eu>
2023-05-19 16:08:41 +0300

    Hide stack trace if no help found

--- .config/nvim/lua/modules/utils/init.lua
+++ .config/nvim/lua/modules/utils/init.lua
@@ -1,9 +1,13 @@
 vim.api.nvim_create_user_command('HelpAutosplit',
     function(opts)
+        local err
         if vim.fn.winwidth('%') > vim.fn.winheight('%') then
-            vim.cmd([[vertical help ]] .. opts.fargs[1])
+            err = pcall(function() vim.cmd([[vertical help ]] .. opts.fargs[1]) end)
         else
-            vim.cmd([[help ]] .. opts.fargs[1])
+            err = pcall(function() vim.cmd([[help ]] .. opts.fargs[1]) end)
+        end
+        if not err then
+            print("No help for " .. opts.fargs[1])
         end
     end, { nargs = 1, complete = "help" })
-vim.cmd'cnoreabbrev h HelpAutosplit'
+vim.cmd 'cnoreabbrev h HelpAutosplit'
