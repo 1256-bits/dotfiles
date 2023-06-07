1220f2f8c4a89b05f3cd1309a60a15add1a729ca
128bit <128bit@eclipso.eu>
2023-06-07 13:09:14 +0300

    fix formatting, remove commented bind

--- .config/nvim/lua/modules/remaps/init.lua
+++ .config/nvim/lua/modules/remaps/init.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<Leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<Leader>/", function()
-	vim.cmd.let("@/=''")
+    vim.cmd.let("@/=''")
 end)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -15,9 +15,9 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 
@@ -29,5 +29,3 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
-
---vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
