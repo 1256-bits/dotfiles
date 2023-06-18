fd0e6e1246e954f98bb492938094f1dcdbe2da5b
128bit <128bit@eclipso.eu>
2023-06-18 13:48:05 +0300

    Add doom emacs configuration

---
+++ .doom.d/custom.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(reverse-im-input-methods '("russian-computer")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
