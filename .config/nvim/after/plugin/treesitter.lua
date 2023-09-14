ec3b4ce23a102dcb4d5d7b3e849fba887d74a36e
128bit <128bit@eclipso.eu>
2023-09-14 16:31:30 +0300

    Add nushell parser

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
     ensure_installed = { "javascript", "typescript", "bash", "c", "lua", "vim", "vimdoc", "query", "html", "sxhkdrc",
-        "scss", },
+        "scss", "nu", },
     sync_install = false,
     auto_install = true,
     highlight = {
