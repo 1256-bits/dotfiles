380d166dbc0468e84ab0f035736b8f29f9d9ddba
128bit <128bit@eclipso.eu>
2023-11-14 14:47:32 +0300

    Add git push keybind

--- .config/nvim/lua/modules/remaps/init.lua
+++ .config/nvim/lua/modules/remaps/init.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<Leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<Leader>/", function()
-    vim.cmd.let("@/=''")
+  vim.cmd.let("@/=''")
 end)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -27,7 +27,11 @@ vim.keymap.set("n", "<leader>s", [[:%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+  vim.cmd("so")
 end)
 
 vim.keymap.set("n", "<leader>w", "<C-W>")
+
+vim.keymap.set({ "n", "v" }, "<leader>gg", function()
+  vim.cmd('G push')
+end)
