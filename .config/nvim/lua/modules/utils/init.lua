78331d5f785cf3d6cb96746b360e8302c783fb57
128bit <128bit@eclipso.eu>
2023-11-19 13:30:45 +0300

    rename err to ok

--- .config/nvim/lua/modules/utils/init.lua
+++ .config/nvim/lua/modules/utils/init.lua
@@ -1,12 +1,12 @@
 vim.api.nvim_create_user_command('HelpAutosplit',
     function(opts)
-        local err
+        local ok
         if vim.fn.winwidth('%') > 100 then
-            err = pcall(function() vim.cmd([[vertical help ]] .. opts.fargs[1]) end)
+            ok = pcall(function() vim.cmd([[vertical help ]] .. opts.fargs[1]) end)
         else
-            err = pcall(function() vim.cmd([[help ]] .. opts.fargs[1]) end)
+            ok = pcall(function() vim.cmd([[help ]] .. opts.fargs[1]) end)
         end
-        if not err then
+        if not ok then
             print("No help for " .. opts.fargs[1])
         end
     end, { nargs = 1, complete = "help" })
