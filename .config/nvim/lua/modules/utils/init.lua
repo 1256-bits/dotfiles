223ec42fecab0e21144fcff868592c255397170c
128bit <128bit@eclipso.eu>
2023-11-19 14:02:04 +0300

    Allow to call HelpAutoSplit with no arguments

--- .config/nvim/lua/modules/utils/init.lua
+++ .config/nvim/lua/modules/utils/init.lua
@@ -1,13 +1,14 @@
 vim.api.nvim_create_user_command('HelpAutosplit',
     function(opts)
+        local arg = opts.fargs[1] and opts.fargs[1] or ""
         local ok
         if vim.fn.winwidth('%') > 100 then
-            ok = pcall(function() vim.cmd([[vertical help ]] .. opts.fargs[1]) end)
+            ok = pcall(function() vim.cmd([[vertical help ]] .. arg) end)
         else
-            ok = pcall(function() vim.cmd([[help ]] .. opts.fargs[1]) end)
+            ok = pcall(function() vim.cmd([[help ]] .. arg) end)
         end
         if not ok then
             print("No help for " .. opts.fargs[1])
         end
-    end, { nargs = 1, complete = "help" })
+    end, { nargs = "?", complete = "help" })
 vim.cmd 'cnoreabbrev h HelpAutosplit'
