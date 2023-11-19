de9ea393eb4c23973a0fb173c6fcfec1110df84f
128bit <128bit@eclipso.eu>
2023-11-19 21:23:05 +0300

    Add new plugins

--- .config/nvim/lua/modules/lazy/init.lua
+++ .config/nvim/lua/modules/lazy/init.lua
@@ -1,75 +1,82 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    'kovetskiy/sxhkd-vim',
+  'kovetskiy/sxhkd-vim',
+  'VonHeikemen/lsp-zero.nvim',
+  'ThePrimeagen/harpoon',
+  'mbbill/undotree',
+  'hrsh7th/cmp-path',
+  'saadparwaiz1/cmp_luasnip',
+  'hrsh7th/cmp-buffer',
+  'tpope/vim-fugitive',
+  'shaunsingh/nord.nvim',
+  'EdenEast/nightfox.nvim',
+  'mfussenegger/nvim-dap',
+  'LhKipp/nvim-nu',
+  "dstein64/vim-startuptime",
+  "mattn/efm-langserver",
+  {
+    'creativenull/efmls-configs-nvim',
+    version = 'v1.x.x', -- version is optional, but recommended
+    dependencies = { 'neovim/nvim-lspconfig' },
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap' }
+  },
+  {
+    'mcchrish/zenbones.nvim',
+    lazy = true,
+    dependencies = { 'rktjmp/lush.nvim' }
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.4',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
     'VonHeikemen/lsp-zero.nvim',
-    'ThePrimeagen/harpoon',
-    'mbbill/undotree',
-    'hrsh7th/cmp-path',
-    'saadparwaiz1/cmp_luasnip',
-    'hrsh7th/cmp-buffer',
-    'tpope/vim-fugitive',
-    'shaunsingh/nord.nvim',
-    'jose-elias-alvarez/null-ls.nvim',
-    'EdenEast/nightfox.nvim',
-    'mfussenegger/nvim-dap',
-    'LhKipp/nvim-nu',
-    {
-        'rcarriga/nvim-dap-ui',
-        dependencies = { 'mfussenegger/nvim-dap' }
-    },
-    {
-        'mcchrish/zenbones.nvim',
-        dependencies = { 'rktjmp/lush.nvim' }
-    },
-    {
-        "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
-    },
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.4',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            {
-                -- Optional
-                'williamboman/mason.nvim',
-                build = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' }, -- Required
+      {
+        -- Optional
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
-        }
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },     -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'L3MON4D3/LuaSnip' },     -- Required
     }
+  }
 }
 
 local opts = {}
