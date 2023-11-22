772ad2d004f55788c7ca5b424c3a6ba77e1e2f2d
128bit <128bit@eclipso.eu>
2023-11-22 18:24:30 +0300

    formatting

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,24 +1,24 @@
-local lsp = require('lsp-zero').preset({})
-local cmp = require('cmp')
-local lspconfig = require('lspconfig')
+local lsp = require("lsp-zero").preset({})
+local cmp = require("cmp")
+local lspconfig = require("lspconfig")
 
 --Set up lsp
 
 lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({ buffer = bufnr })
+  lsp.default_keymaps({ buffer = bufnr })
 end)
 
 lsp.ensure_installed({
-    'lua_ls',
-    'bashls',
-    'tsserver',
-    'emmet_ls',
-    'yamlls',
-    'html',
-    'cssls',
-    'cssmodules_ls',
-    'fennel_language_server',
-    'pylsp',
+  "lua_ls",
+  "bashls",
+  "tsserver",
+  "emmet_ls",
+  "yamlls",
+  "html",
+  "cssls",
+  "cssmodules_ls",
+  "fennel_language_server",
+  "pylsp",
 })
 -- (Optional) Configure lua language server for neovim
 lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
@@ -28,63 +28,63 @@ lsp.setup()
 --Set up completion
 
 cmp.setup({
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'path' },
-        { name = 'buffer' },
-        { name = 'luasnip' },
-    },
-    mapping = {
-        ['<C-n'] = cmp.mapping.select_next_item(),
-        ['<C-p'] = cmp.mapping.select_prev_item(),
-    }
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "path" },
+    { name = "buffer" },
+    { name = "luasnip" },
+  },
+  mapping = {
+    ["<C-n"] = cmp.mapping.select_next_item(),
+    ["<C-p"] = cmp.mapping.select_prev_item(),
+  },
 })
 
 --LSP modules configuration--
 
-lspconfig.cssmodules_ls.setup {
-    -- provide your on_attach to bind keymappings
-    on_attach = custom_on_attach,
-    -- optionally
-    init_options = {
-        camelCase = 'dashes',
-    },
-}
+lspconfig.cssmodules_ls.setup({
+  -- provide your on_attach to bind keymappings
+  on_attach = custom_on_attach,
+  -- optionally
+  init_options = {
+    camelCase = "dashes",
+  },
+})
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-lspconfig.cssls.setup {
-    capabilities = capabilities,
-}
-
-local configs = require('lspconfig/configs')
-
-lspconfig.emmet_ls.setup({
-    init_options = {
-        html = {
-            options = {
-                ['output.format'] = true,
-                ['output.inlineBreak'] = 2,
-                ["bem.enabled"] = true,
-            },
-        },
-    }
+lspconfig.cssls.setup({
+  capabilities = capabilities,
 })
 
-lspconfig.pylsp.setup {
-    settings = {
-        pylsp = {
-            plugins = {
-                pycodestyle = {
-                    maxLineLength = 100,
-                },
-                jedi = {
-                    environment = vim.fn.expand("$HOME") .. "/.local/lib/python3.11/"
-                }
-            }
-        }
-    }
-}
+local configs = require("lspconfig/configs")
+
+lspconfig.emmet_ls.setup({
+  init_options = {
+    html = {
+      options = {
+        ["output.format"] = true,
+        ["output.inlineBreak"] = 2,
+        ["bem.enabled"] = true,
+      },
+    },
+  },
+})
+
+lspconfig.pylsp.setup({
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          maxLineLength = 100,
+        },
+        jedi = {
+          environment = vim.fn.expand("$HOME") .. "/.local/lib/python3.11/",
+        },
+      },
+    },
+  },
+})
 
 lsp.setup()
