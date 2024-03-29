a7a12920199c38537ad70ba3dab7a8dcba295207
128bit <128bit@eclipso.eu>
2024-03-29 15:56:56 +0300

    change formatting

--- .config/nvim/lua/modules/utils/init.lua
+++ .config/nvim/lua/modules/utils/init.lua
@@ -1,14 +1,17 @@
-vim.api.nvim_create_user_command('HelpAutosplit',
-    function(opts)
-        local arg = opts.fargs[1] and opts.fargs[1] or ""
-        local ok
-        if vim.fn.winwidth('%') > 100 then
-            ok = pcall(function() vim.cmd([[vertical help ]] .. arg) end)
-        else
-            ok = pcall(function() vim.cmd([[help ]] .. arg) end)
-        end
-        if not ok then
-            print("No help for " .. opts.fargs[1])
-        end
-    end, { nargs = "?", complete = "help" })
-vim.cmd 'cnoreabbrev h HelpAutosplit'
+vim.api.nvim_create_user_command("HelpAutosplit", function(opts)
+  local arg = opts.fargs[1] and opts.fargs[1] or ""
+  local ok
+  if vim.fn.winwidth("%") > 100 then
+    ok = pcall(function()
+      vim.cmd([[vertical help ]] .. arg)
+    end)
+  else
+    ok = pcall(function()
+      vim.cmd([[help ]] .. arg)
+    end)
+  end
+  if not ok then
+    print("No help for " .. opts.fargs[1])
+  end
+end, { nargs = "?", complete = "help" })
+vim.cmd("cnoreabbrev h HelpAutosplit")
