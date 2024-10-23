56d763a065107c1633eaef65f24814ab03b6cce4
128bit <128bit@eclipso.eu>
2024-10-23 19:17:06 +0300

    Use tsx for typescript, add node2 profile

--- .config/nvim/after/plugin/dap-cfg.lua
+++ .config/nvim/after/plugin/dap-cfg.lua
@@ -61,6 +61,12 @@ require("dap-vscode-js").setup({
   -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
 })
 
+dap.adapters.node2 = {
+  type = "executable",
+  command = "node",
+  args = { os.getenv("HOME") .. "/test/vscode-node-debug2/out/src/nodeDebug.js" },
+}
+
 for _, language in ipairs({ "typescript", "javascript" }) do
   local is_yarn = vim.fn.glob(".yarn") ~= ""
   local executable = is_yarn and "yarn" or "node"
@@ -69,7 +75,7 @@ for _, language in ipairs({ "typescript", "javascript" }) do
     if is_yarn then
       runtimeArgs = { "node", "-r", "ts-node/register" }
     else
-      runtimeArgs = { "-r", "ts-node/register" }
+      runtimeArgs = { "--import", "tsx" }
     end
   end
   require("dap").configurations[language] = {
@@ -125,6 +131,23 @@ for _, language in ipairs({ "typescript", "javascript" }) do
       url = "http://localhost:3000",
       webRoot = "${workspaceFolder}",
       userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+    },
+    {
+      name = "Launch",
+      type = "node2",
+      request = "launch",
+      program = "${file}",
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+      protocol = "inspector",
+      console = "integratedTerminal",
+    },
+    {
+      -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+      name = "Attach to process",
+      type = "node2",
+      request = "attach",
+      processId = require("dap.utils").pick_process,
     },
   }
 end
